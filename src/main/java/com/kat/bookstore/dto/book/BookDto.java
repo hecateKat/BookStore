0ef5e3570a394e4df057868928216d8d2ac5b10d
Katarzyna Ochmanska <kat.ochmanska@gmail.com>
2025-05-10 19:24:34 +0200

    Solution - Spring Boot Portfolio Project
    not working

--- src/main/java/com/kat/bookstore/dto/book/BookDto.java
+++ src/main/java/com/kat/bookstore/dto/book/BookDto.java
@@ -2,11 +2,15 @@ package com.kat.bookstore.dto.book;
 
 import java.math.BigDecimal;
 import java.util.Set;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookDto {
     private Long id;
     private String title;
---
+++ src/test/resources/book/repository/add-category.sql
@@ -0,0 +1,9 @@
+INSERT INTO categories VALUES
+                           (1, 'One', 'category 1', 0),
+                           (2, 'Two', 'category 2', 0),
+                           (3, 'Three', 'category 3', 0);
+INSERT INTO books_categories VALUES
+                                 (2, 2),
+                                 (3, 2),
+                                 (4, 2),
+                                 (5, 2);
